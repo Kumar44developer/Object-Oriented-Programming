488336472df5bb9595f7febcf15bbb2cb93889e5
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-20 12:14:50 +0530

    Added Student class with display method

--- program1.cpp
+++ program1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    void display() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
 
