721f324a6f98f7c83421d33b960f3d6d4451c191
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-21 11:33:05 +0530

    Implemented constructor and destructor in Person class
    Added constructor and destructor to Person class.

--- program2.cpp
+++ program2.cpp
@@ -5,4 +5,21 @@ class Person {
     string name;
     int age;
 
+public:
+    // Constructor
+    Person(string n, int a) {
+        name = n;
+        age = a;
+        cout << "Constructor called\n";
+    }
+
+    void show() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+
+    // Destructor
+    ~Person() {
+        cout << "Destructor called for " << name << endl;
+    }
+};
 
