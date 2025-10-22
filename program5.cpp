b4cc0ffa57ec5289325929c7fc8bf289a1831a30
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-22 11:37:29 +0530

    Updated program5.cpp by Demonstrating the Method Overloading

--- program5.cpp
+++ program5.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Print {
+public:
+    void show(int a) {
+        cout << "Integer: " << a << endl;
+    }
+    void show(double a) {
+        cout << "Double: " << a << endl;
+    }
+    void show(string a) {
+        cout << "String: " << a << endl;
+    }
+};
+
+
 
