7fbbe294f9385e216312ba898d2f8996675aa4e9
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-24 09:14:58 +0530

    created a function named as a show and demonstrated the concept of overloading

--- program7.cpp
+++ program7.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Print {
+public:
+    void show(int a) {
+        cout << "Integer: " << a << endl;
+    }
+
+    void show(double b) {
+        cout << "Double: " << b << endl;
+    }
+
+    void show(string c) {
+        cout << "String: " << c << endl;
+    }
+};
 
